e1a3f1dedc37948bc6a4a0accccb8ddd6fe65338
Ishwarya-24 <gishwarya24@gmail.com>
2023-07-05 13:59:09 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/unittests.py
@@ -0,0 +1,34 @@
+import unittest
+from machinetranslation.translator import englishToFrench, frenchToEnglish
+
+
+
+
+class TranslationTests(unittest.TestCase):
+    def test_english_to_french_hello(self):
+        english_text = 'Hello'
+        translated_text = englishToFrench(english_text)
+        expected_text = 'Bonjour'
+        self.assertEqual(translated_text, expected_text)
+
+    def test_english_to_french_bonjour(self):
+        english_text = 'Bonjour'
+        translated_text = englishToFrench(english_text)
+        expected_text = 'Bonjour'
+        self.assertEqual(translated_text, expected_text)
+
+    def test_french_to_english_bonjour(self):
+        french_text = 'Bonjour'
+        translated_text = frenchToEnglish(french_text)
+        expected_text = 'Hello'
+        self.assertEqual(translated_text, expected_text)
+
+    def test_french_to_english_hello(self):
+        french_text = 'Hello'
+        translated_text = frenchToEnglish(french_text)
+        expected_text = 'Hello'
+        self.assertEqual(translated_text, expected_text)
+
+if __name__ == '__main__':
+    unittest.main()
+
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,23 @@
-from machinetranslation import translator
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, jsonify
+from machinetranslation.translator import englishToFrench, frenchToEnglish
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+def translate_english_to_french():
+    text_to_translate = request.args.get('textToTranslate')
+    translated_text = englishToFrench(text_to_translate)
+    return jsonify({'translated_text': translated_text})
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+def translate_french_to_english():
+    text_to_translate = request.args.get('textToTranslate')
+    translated_text = frenchToEnglish(text_to_translate)
+    return jsonify({'translated_text': translated_text})
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+def render_index_page():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
